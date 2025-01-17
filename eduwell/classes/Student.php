915501231a6ca3fdb301341748fb0efdef7f2b65
Anna_easysmile <160872733+annaakunevych@users.noreply.github.com>
2025-01-17 16:46:34 +0100

    Update Student.php

--- eduwell/classes/Student.php
+++ eduwell/classes/Student.php
@@ -16,7 +16,7 @@ class Student
 
     public function __construct($id, $nazov_kurzu, $datum_kurzu, $meno, $priezvisko, $pohlavie, $vek, $mesto_bydliska, $stav_absolvovania)
     {
-       // $this->id = $id;
+        $this->id = $id;
         $this->nazov_kurzu = $nazov_kurzu;
         $this->datum_kurzu = $datum_kurzu;
         $this->meno = $meno;
